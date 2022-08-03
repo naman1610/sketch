cc23eb354de0eac60339090a99dd5adcd39047f5
Naman <naman16102003@gmail.com>
2022-08-03 22:33:46 +0530

    Change color of boxes on and after hover

--- js/script.js
+++ js/script.js
@@ -9,3 +9,11 @@ str = '<div class="vertical">'+vertical.innerHTML+'</div>';
 for (let j = 0; j < 16; j++) {
     head.innerHTML += str;
 }
+
+// Changing color of the squares on hover
+let squares = document.getElementsByClassName('horizontal');
+for (let i = 0; i < squares.length; i++) {
+    squares[i].addEventListener('mouseover', function () {
+        squares[i].style.backgroundColor = 'red';
+    });
+}
