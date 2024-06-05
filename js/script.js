24be25779a404a8472001e0447c63e777127b2c6
Naman <naman16102003@gmail.com>
2024-06-05 13:30:16 +0530

    Added functionality to change grid size and randomized filling of colors in grid

--- js/script.js
+++ js/script.js
@@ -1,19 +1,28 @@
-let head = document.getElementById('container');
-let vertical = document.createElement('div');
-vertical.className = 'vertical';
-let str = "<div class='horizontal'></div>";
-for (let i = 0; i < 16; i++) {
-    vertical.innerHTML += str;
-}
-str = '<div class="vertical">'+vertical.innerHTML+'</div>';
-for (let j = 0; j < 16; j++) {
-    head.innerHTML += str;
-}
+function createGrid() {
+    let size = prompt("Enter number of squares in a row");
+    if (size > 100 || size < 0) {
+        size = 100;
+    }
+    let head = document.getElementById("container");
+    head.innerHTML = "";
+    let vertical = document.createElement("div");
+    vertical.className = "vertical";
+    let str = "<div class='horizontal'></div>";
+    for (let i = 0; i < size; i++) {
+        vertical.innerHTML += str;
+    }
+    str = '<div class="vertical">' + vertical.innerHTML + "</div>";
+    for (let j = 0; j < size; j++) {
+        head.innerHTML += str;
+    }
+    let squares = document.getElementsByClassName("horizontal");
+    for (let i = 0; i < squares.length; i++) {
+        squares[i].addEventListener("mouseover", function () {
+            let r = Math.floor(Math.random() * 256);
+            let g = Math.floor(Math.random() * 256);
+            let b = Math.floor(Math.random() * 256);
 
-// Changing color of the squares on hover
-let squares = document.getElementsByClassName('horizontal');
-for (let i = 0; i < squares.length; i++) {
-    squares[i].addEventListener('mouseover', function () {
-        squares[i].style.backgroundColor = 'red';
-    });
+            squares[i].style.backgroundColor = `rgb(${r},${g},${b})`;
+        });
+    }
 }
